22bf20f4247ac923374a67448d438c75c55e452f
Lazar4o <lazar_2003@yahoo.com>
2024-10-10 14:32:52 +0300

    Added HINT because i couldnt cause the flat list to rerender

--- src/pages/TasksList/TasksList.tsx
+++ src/pages/TasksList/TasksList.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { TextInput, FlatList } from 'react-native';
+import { TextInput, FlatList, ActivityIndicator } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   selectFilteredTasks,
@@ -64,125 +64,136 @@ const TaskList: FC = () => {
 
   return (
     <View padding-10>
-      {/* Radio Group for Status Filter */}
-      <RadioGroup
-        initialValue={selectedStatus}
-        onValueChange={handleStatusChange}
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          marginBottom: 10,
-        }}>
-        <RadioButton value="all" label="All" />
-        <RadioButton value="completed" label="Completed" />
-        <RadioButton value="notCompleted" label="Incomplete" />
-      </RadioGroup>
+      <Text center marginV-5>HINT: Initially To re-render flat list - Change some filter</Text>
+      {/* Loading indicator */}
+      {loading ? (
+        <ActivityIndicator/>
+      ) : (
+        <>
+          {/* Radio Group for Status Filter */}
+          <RadioGroup
+            initialValue={selectedStatus}
+            onValueChange={handleStatusChange}
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginBottom: 10,
+            }}>
+            <RadioButton value="all" label="All" />
+            <RadioButton value="completed" label="Completed" />
+            <RadioButton value="notCompleted" label="Incomplete" />
+          </RadioGroup>
 
-      {/* Filter by Title */}
-      <TextInput
-        style={{
-          padding: 10,
-          backgroundColor: 'white',
-          borderColor: 'black',
-          borderWidth: 1,
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        placeholder="Filter by title"
-        value={filters.title}
-        onChangeText={(text: string) => handleFilterChange('title', text)}
-      />
+          {/* Filter by Title */}
+          <TextInput
+            style={{
+              padding: 10,
+              backgroundColor: 'white',
+              borderColor: 'black',
+              borderWidth: 1,
+              borderRadius: 10,
+              marginBottom: 10,
+            }}
+            placeholder="Filter by title"
+            value={filters.title}
+            onChangeText={(text: string) => handleFilterChange('title', text)}
+          />
 
-      {/* Filter by User ID */}
-      <TextInput
-        style={{
-          padding: 10,
-          backgroundColor: 'white',
-          borderColor: 'black',
-          borderWidth: 1,
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        placeholder="Filter by User ID"
-        keyboardType="numeric"
-        value={filters.userId ? filters.userId.toString() : ''}
-        onChangeText={(text: string) =>
-          handleFilterChange('userId', text ? parseInt(text, 10) : null)
-        }
-      />
+          {/* Filter by User ID */}
+          <TextInput
+            style={{
+              padding: 10,
+              backgroundColor: 'white',
+              borderColor: 'black',
+              borderWidth: 1,
+              borderRadius: 10,
+              marginBottom: 10,
+            }}
+            placeholder="Filter by User ID"
+            keyboardType="numeric"
+            value={filters.userId ? filters.userId.toString() : ''}
+            onChangeText={(text: string) =>
+              handleFilterChange('userId', text ? parseInt(text, 10) : null)
+            }
+          />
 
-      {/* Reset Filters */}
-      <TouchableOpacity
-        bg-blue40
-        style={{
-          borderColor: 'black',
-          borderWidth: 1,
-          borderRadius: 5,
-          padding: 5,
-          margin: 5,
-        }}
-        onPress={handleResetFilters}>
-        <Text center white>
-          Reset Filters
-        </Text>
-      </TouchableOpacity>
+          {/* Reset Filters */}
+          <TouchableOpacity
+            bg-blue40
+            style={{
+              borderColor: 'black',
+              borderWidth: 1,
+              borderRadius: 5,
+              padding: 5,
+              margin: 5,
+            }}
+            onPress={handleResetFilters}>
+            <Text center white>
+              Reset Filters
+            </Text>
+          </TouchableOpacity>
 
-      {/* Tasks List */}
-      <FlatList
-        data={paginatedTasks}
-        extraData={[tasks, filters, pagination]}
-        keyExtractor={item => item.id.toString()}
-        style={{
-          maxHeight: 350,
-        }}
-        renderItem={({ item }) => (
+          {/* Tasks List */}
+          <FlatList
+            data={paginatedTasks}
+            extraData={[tasks, filters, pagination]}
+            keyExtractor={item => item.id.toString()}
+            style={{
+              maxHeight: 350,
+            }}
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item.title}</Text>
+                <Text>Owner ID: {item.userId}</Text>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: item.completed ? 'green' : 'red',
+                  }}
+                  onPress={() =>
+                    dispatch(
+                      updateTaskStatus({
+                        id: item.id,
+                        status: !item.completed,
+                      }),
+                    )
+                  }>
+                  <Text>
+                    {item.completed ? 'Mark Incomplete' : 'Mark Complete'}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+
+          {/* Pagination Controls */}
           <View>
-            <Text>{item.title}</Text>
-            <Text>Owner ID: {item.userId}</Text>
             <TouchableOpacity
               style={{
-                backgroundColor: item.completed ? 'green' : 'red',
+                borderColor: 'black',
+                borderWidth: 1,
+                borderRadius: 5,
+                padding: 5,
+                margin: 5,
               }}
-              onPress={() =>
-                dispatch(
-                  updateTaskStatus({ id: item.id, status: !item.completed }),
-                )
-              }>
-              <Text>
-                {item.completed ? 'Mark Incomplete' : 'Mark Complete'}
-              </Text>
+              disabled={pagination.currentPage === 1}
+              onPress={() => handlePageChange(pagination.currentPage - 1)}>
+              <Text center>Previous</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                borderColor: 'black',
+                borderWidth: 1,
+                borderRadius: 5,
+                padding: 5,
+                margin: 5,
+              }}
+              disabled={paginatedTasks.length < pagination.pageSize}
+              onPress={() => handlePageChange(pagination.currentPage + 1)}>
+              <Text center>Next</Text>
             </TouchableOpacity>
           </View>
-        )}
-      />
-
-      {/* Pagination Controls */}
-      <View>
-        <TouchableOpacity
-          style={{
-            borderColor: 'black',
-            borderWidth: 1,
-            borderRadius: 5,
-            padding: 5,
-            margin: 5,
-          }}
-          disabled={pagination.currentPage === 1}
-          onPress={() => handlePageChange(pagination.currentPage - 1)}>
-          <Text center>Previous</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            borderColor: 'black',
-            borderWidth: 1,
-            borderRadius: 5,
-            padding: 5,
-            margin: 5,
-          }}
-          disabled={paginatedTasks.length < pagination.pageSize}
-          onPress={() => handlePageChange(pagination.currentPage + 1)}>
-          <Text center>Next</Text>
-        </TouchableOpacity>
-      </View>
+        </>
+      )}
     </View>
   );
 };
